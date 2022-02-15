0328a8377ad38040e3075cc7ec6f73735184be99
Daniel-Digid <daniel@dig.id>
2022-02-15 14:51:57 +0000

    Dev

---
+++ page-templates/page-home.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Template Name: Home Page Template
+ *
+ * The template for displaying all content of the homepage
+ */
+
+get_header();
+shs_theme_before_main_content(); ?>
+
+
+
+<?php
+shs_theme_after_main_content();
+get_footer();
