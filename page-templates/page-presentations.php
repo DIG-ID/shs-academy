fa62c09ebe5fe36b1a14963f07e8ee2c2c5f2df0
Pedro Silva <pedro@dig.id>
2022-03-10 11:31:57 +0000

    up

---
+++ page-templates/page-presentations.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Template Name: Presentations Page Template
+ *
+ * The template for displaying all content of the presentations page
+ */
+
+get_header();
+	get_template_part( 'template-parts/presentations/content' );
+get_footer();
---
+++ template-parts/presentations/content.php
@@ -0,0 +1,127 @@
+<main>
+	<section class="page-intro">
+		<div class="container">
+			<div class="row justify-content-center align-content-center">
+				<div class="col-6 text-center">
+					<p class="page-intro__content">Here you will find the approved presentations
+	from the speakers, which you experienced live
+	on the Swiss Innovation Day 2021. </p>
+				</div>
+			</div>
+		</div>
+	</section>
+	<section class="presentation-cards">
+		<div class="container container__inside">
+			<div class="row">
+				<article class="presentation-card col-12 col-sm-12 col-md-6 col-xl-4 col-xxl-4">
+					<header class="">
+						<img src="https://picsum.photos/370/370" alt="image">
+						<span class="presentation-card__category">Category</span>
+						<h2 class="presentation-card__title">Person Name</h2>
+						<p class="presentation-card__position">Person Position</p>
+					</header>
+					<span class="presentation-card__separator"></span>
+					<p class="presentation-card__description">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantiutotam rem aperiam eaque ipsa quae  inventore</p>
+					<ul class="presentation-card__links">
+						<li><a href="">link 1</a></li>
+						<li><a href="">link 2</a></li>
+						<li><a href="">link 3</a></li>
+					</ul>
+					<footer>
+						<a href="" target="_blank" class="presentation-card__pdf main-btn main-btn__red">Download PDF</a>
+					</footer>
+				</article>
+				<article class="presentation-card col-12 col-sm-12 col-md-6 col-xl-4 col-xxl-4">
+					<header class="">
+						<img src="https://picsum.photos/370/370" alt="image">
+						<span class="presentation-card__category">Category</span>
+						<h2 class="presentation-card__title">Person Name</h2>
+						<p class="presentation-card__position">Person Position</p>
+					</header>
+					<span class="presentation-card__separator"></span>
+					<p class="presentation-card__description">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantiutotam rem aperiam eaque ipsa quae  inventore</p>
+					<ul class="presentation-card__links">
+						<li><a href="">link 1</a></li>
+						<li><a href="">link 2</a></li>
+						<li><a href="">link 3</a></li>
+					</ul>
+					<footer>
+						<a href="" target="_blank" class="presentation-card__pdf main-btn main-btn__red">Download PDF</a>
+					</footer>
+				</article>
+				<article class="presentation-card col-12 col-sm-12 col-md-6 col-xl-4 col-xxl-4">
+					<header class="">
+						<img src="https://picsum.photos/370/370" alt="image">
+						<span class="presentation-card__category">Category</span>
+						<h2 class="presentation-card__title">Person Name</h2>
+						<p class="presentation-card__position">Person Position</p>
+					</header>
+					<span class="presentation-card__separator"></span>
+					<p class="presentation-card__description">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantiutotam rem aperiam eaque ipsa quae  inventore</p>
+					<ul class="presentation-card__links">
+						<li><a href="">link 1</a></li>
+						<li><a href="">link 2</a></li>
+						<li><a href="">link 3</a></li>
+					</ul>
+					<footer>
+						<a href="" target="_blank" class="presentation-card__pdf main-btn main-btn__red">Download PDF</a>
+					</footer>
+				</article>
+				<article class="presentation-card col-12 col-sm-12 col-md-6 col-xl-4 col-xxl-4">
+					<header class="">
+						<img src="https://picsum.photos/370/370" alt="image">
+						<span class="presentation-card__category">Category</span>
+						<h2 class="presentation-card__title">Person Name</h2>
+						<p class="presentation-card__position">Person Position</p>
+					</header>
+					<span class="presentation-card__separator"></span>
+					<p class="presentation-card__description">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantiutotam rem aperiam eaque ipsa quae  inventore</p>
+					<ul class="presentation-card__links">
+						<li><a href="">link 1</a></li>
+						<li><a href="">link 2</a></li>
+						<li><a href="">link 3</a></li>
+					</ul>
+					<footer>
+						<a href="" target="_blank" class="presentation-card__pdf main-btn main-btn__red">Download PDF</a>
+					</footer>
+				</article>
+				<article class="presentation-card col-12 col-sm-12 col-md-6 col-xl-4 col-xxl-4">
+					<header class="">
+						<img src="https://picsum.photos/370/370" alt="image">
+						<span class="presentation-card__category">Category</span>
+						<h2 class="presentation-card__title">Person Name</h2>
+						<p class="presentation-card__position">Person Position</p>
+					</header>
+					<span class="presentation-card__separator"></span>
+					<p class="presentation-card__description">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantiutotam rem aperiam eaque ipsa quae  inventore</p>
+					<ul class="presentation-card__links">
+						<li><a href="">link 1</a></li>
+						<li><a href="">link 2</a></li>
+						<li><a href="">link 3</a></li>
+					</ul>
+					<footer>
+						<a href="" target="_blank" class="presentation-card__pdf main-btn main-btn__red">Download PDF</a>
+					</footer>
+				</article>
+				<article class="presentation-card col-12 col-sm-12 col-md-6 col-xl-4 col-xxl-4">
+					<header class="">
+						<img src="https://picsum.photos/370/370" alt="image">
+						<span class="presentation-card__category">Category</span>
+						<h2 class="presentation-card__title">Person Name</h2>
+						<p class="presentation-card__position">Person Position</p>
+					</header>
+					<span class="presentation-card__separator"></span>
+					<p class="presentation-card__description">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantiutotam rem aperiam eaque ipsa quae  inventore</p>
+					<ul class="presentation-card__links">
+						<li><a href="">link 1</a></li>
+						<li><a href="">link 2</a></li>
+						<li><a href="">link 3</a></li>
+					</ul>
+					<footer>
+						<a href="" target="_blank" class="presentation-card__pdf main-btn main-btn__red">Download PDF</a>
+					</footer>
+				</article>
+			</div>
+		</div>
+	</section>
+</main>
