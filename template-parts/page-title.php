e847810b7a3d13c75311abac5817c0806a0cc9bb
Daniel-Digid <daniel@dig.id>
2022-03-25 12:38:42 +0000

    Dev

--- template-parts/page-title.php
+++ template-parts/page-title.php
@@ -2,7 +2,7 @@
 <div id="wrapper-banner">
 	<div class="container">
 		<div class="row">
-			<div class="col-12 col-sm-12 col-md-12 px-15 page__title"><?php the_title(); ?></div>
+			<div class="col-12 col-sm-12 col-md-12 px-15"><h1 class="page__title"><?php the_title(); ?></h1></div>
 		</div>
 	</div>
 </div>
