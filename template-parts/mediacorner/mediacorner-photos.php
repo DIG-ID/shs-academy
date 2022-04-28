539b0a0302e93dfc000f74ec5e76f08076925c61
pedro-digid <pedro@dig.id>
2022-04-28 11:26:47 +0100

    up

--- template-parts/mediacorner/mediacorner-photos.php
+++ template-parts/mediacorner/mediacorner-photos.php
@@ -2,17 +2,17 @@
 	<div class="container container__inside">
 		<div class="row">
 			<div class="col-12 col-sm-12 col-md-6 col-lg-12 col-xl-6 px-15">
-				<p class="mediacorner__title"><?php echo get_field('photos_title'); ?></p>
+				<p class="mediacorner__title"><?php echo get_field( 'photos_title' ); ?></p>
 			</div>
 			<div class="d-flex col-12 col-sm-12 col-md-6 col-lg-12 col-xl-6 px-15 justify-content-xl-end">
 				<p class="text-xl-left">
-					<?php shs_download_files( 'logos_images' ); ?>
+					<?php shs_download_files( 'photos' ); ?>
 				</p>
 			</div>
 		</div>
 		<div class="row row__photos">
 			<?php
-				$photosImages = get_field('photos');
+				$photosImages = get_field( 'photos' );
 				if( $photosImages ): ?>
 					<?php foreach( $photosImages as $photoImage ): ?>
 						<div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-3 d-flex align-items-center">
