302e7e907d6373d87598c48bc2142e0f89b127d3
pedro-digid <pedro@dig.id>
2022-04-28 12:12:04 +0100

    up

--- src/js/custom/fancybox.js
+++ src/js/custom/fancybox.js
@@ -3,3 +3,15 @@ import { Fancybox } from "@fancyapps/ui/src/Fancybox/Fancybox.js";
 Fancybox.bind("[data-fancybox]", {
   // Your options go here
 });
+
+$(document).on( 'ready', function() {
+  $('.press-article__btn').fancybox({
+      openEffect: 'elastic',
+      closeEffect: 'elastic',
+      autoSize: true,
+      type: 'iframe',
+      iframe: {
+          preload: false // fixes issue with iframe and IE
+      }
+  });
+});
--- src/js/custom/sticky-navbar.js
+++ src/js/custom/sticky-navbar.js
@@ -1,11 +1,16 @@
 $(function() {
   /*Navbar sticky on scroll*/
   $('.navbar-main').after('<div class="navbar-main-bumper"></div>');
-  $(window).on( 'scroll', function(){
-    if($(this).scrollTop() > 60){
-      $('.navbar-main').addClass('sticky')
-    } else{
-      $('.navbar-main').removeClass('sticky')
-    }
-  });
+  if (window.innerWidth >= 992) {
+    $(window).on( 'scroll', function(){
+      if($(this).scrollTop() > 60 ){
+        $('.navbar-main').addClass('sticky');
+      } else{
+        $('.navbar-main').removeClass('sticky');
+      }
+    });
+  } else {
+    $('.navbar-main').addClass('sticky');
+  }
+
 });
