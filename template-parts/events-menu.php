4937394b1027b195951e01d35f5ee9e26eda8adf
Daniel-Digid <daniel@dig.id>
2022-05-10 12:34:33 +0100

    Dev

--- template-parts/events-menu.php
+++ template-parts/events-menu.php
@@ -6,7 +6,7 @@
 					<p class="menu__event-text">Sichere Dir Deinen Platz am #SID22 mit attraktiven Vergünstigungen.</p>
 					<p class="menu__event-button">MEHR ERFAHREN</p>
 			</a>
-			<a class="col-6 col-md-6 col-sm-12 px-15 menu__event-link" href="<?php echo get_permalink( get_page_by_path( 'program' ) ) ?>">
+			<a class="col-6 col-md-6 col-sm-12 px-15 menu__event-link btn disabled disabled-menu-item" href="<?php echo get_permalink( get_page_by_path( 'program' ) ) ?>">
 				<p class="menu__event-title"><?php  echo get_the_title( get_page_by_path( 'program' ) ); ?></p>
 				<p class="menu__event-text">Mit dem Motto «ReDefining Hospitality» dreht sich alles um Talente, Nachhaltigkeit und Hoteltechnologie.</p>
 				<p class="menu__event-button">MEHR ERFAHREN</p>
