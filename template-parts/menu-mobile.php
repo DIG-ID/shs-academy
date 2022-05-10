71d0e7578b946c0e87f8ab4c2ace9661a6f2fb8e
Daniel-Digid <daniel@dig.id>
2022-05-10 11:23:02 +0100

    Dev

--- template-parts/menu-mobile.php
+++ template-parts/menu-mobile.php
@@ -5,30 +5,31 @@
 			array(
 				'theme_location'  => 'megamenu',
 				'container_class' => 'container navbar-collapse mega-navbar',
-				'container_id'    => 'navbar-megamenu',
-				'menu_class'      => 'navbar-nav ml-auto',
-				'fallback_cb'     => '',
-				'menu_id'         => 'mega-menu',
-				'depth'           => 2,
-				'walker'          => new Custom_Walker_Nav_Menu(),
-			)
-		);
-	?>
-	<?php
-		wp_nav_menu(
-			array(
-				'theme_location'  => 'primary',
-				'container_class' => 'container navbar-collapse',
 				'container_id'    => 'navbar-pmenu',
 				'menu_class'      => 'navbar-nav ml-auto',
 				'fallback_cb'     => '',
-				'menu_id'         => 'main-menu',
+				'menu_id'         => 'mega-menu',
 				'depth'           => 2,
 				'walker'          => '',
 			)
 		);
 	?>
 	<div class="container menu__container--mobile">
+	<?php
+		wp_nav_menu(
+			array(
+				'theme_location'  => 'primary',
+				'container_class' => 'navbar-collapse',
+				'container_id'    => 'navbar-mobile-toparea',
+				'menu_class'      => 'navbar-nav ml-auto',
+				'fallback_cb'     => '',
+				'menu_id'         => 'toparea-menu',
+				'depth'           => 2,
+				'walker'          => '',
+			)
+		);
+	?>
+	
 		<?php
 			wp_nav_menu(
 				array(
