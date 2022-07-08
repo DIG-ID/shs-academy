788b12065ea3de88ce270bdf9275dd3bef0c1e9e
Daniel-Digid <daniel@dig.id>
2022-07-08 11:44:21 +0100

    Dev

--- template-parts/home/program.php
+++ template-parts/home/program.php
@@ -13,7 +13,7 @@
 						$file = get_field( 'program_file', $event_program );
 						?>
 						<a class="main-btn main-btn__red btn" href="/program/"><?php esc_html_e( 'Programm entdecken', 'shs-a' ); ?></a>
-						<a class="main-btn main-btn__white btn" href="<?php echo $file; ?>"><?php esc_html_e( 'Download Program', 'shs-a' ); ?></a>
+						<a class="main-btn main-btn__white btn" target="_blank" href="<?php echo $file; ?>"><?php esc_html_e( 'Download Program', 'shs-a' ); ?></a>
 						<?php
 					endif;
 					?>
