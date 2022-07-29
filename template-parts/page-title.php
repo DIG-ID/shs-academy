1afd0d15b62f37e53abe262eee79c071df24d986
Daniel-Digid <daniel@dig.id>
2022-07-29 15:45:58 +0100

    Dev

--- template-parts/page-title.php
+++ template-parts/page-title.php
@@ -5,7 +5,7 @@
 			<div class="col-12 col-sm-12 col-md-12 px-15 page__title-col">
 				<h1 class="page__title"><?php the_title(); ?></h1>
 				<?php if ( is_page('programm') ) : ?>
-					<a href="<?php echo get_field('program_file'); ?>" class="programm__page-sublink">Programm downloaden</a>
+					<a href="<?php echo get_field('program_file', 406); ?>" class="programm__page-sublink">Programm downloaden</a>
 				<?php endif; ?>
 			</div>
 		</div>
