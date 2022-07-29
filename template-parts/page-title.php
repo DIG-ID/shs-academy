c23cae1499da23ff98ca8d2cc70ef9692a3aa041
Daniel-Digid <daniel@dig.id>
2022-07-29 15:40:26 +0100

    Dev

--- template-parts/page-title.php
+++ template-parts/page-title.php
@@ -2,7 +2,12 @@
 <div id="wrapper-banner">
 	<div class="container">
 		<div class="row">
-			<div class="col-12 col-sm-12 col-md-12 px-15"><h1 class="page__title"><?php the_title(); ?></h1></div>
+			<div class="col-12 col-sm-12 col-md-12 px-15 page__title-col">
+				<h1 class="page__title"><?php the_title(); ?></h1>
+				<?php if ( is_page('programm') ) : ?>
+					<a href="<?php echo get_field('program_file'); ?>" class="programm__page-sublink">Programm downloaden</a>
+				<?php endif; ?>
+			</div>
 		</div>
 	</div>
 </div>
