8b7d4500708912f1979902d0f8d8e550b75141df
Daniel-Digid <daniel@dig.id>
2022-08-24 18:18:04 +0100

    Dev

--- template-parts/home/about-us.php
+++ template-parts/home/about-us.php
@@ -13,7 +13,7 @@
 						<?php the_field( 'home_page_settings_about_us_content' ); ?>
 				</div>
 				<p class="visitors-number"><span class="number"><?php the_field( 'home_page_settings_about_us_visitors_number' ); ?></span> <?php esc_html_e( 'Besucher', 'shs-a' ); ?></p>
-				<a  class="main-btn main-btn__red main-btn__mob-centered" href="<?php esc_url( the_field( 'home_page_settings_about_us_button_1_link' ) ); ?>"><?php esc_html_e( 'Tickets kaufen', 'shs-a' ); ?></a>
+				<a  class="main-btn main-btn__red main-btn__mob-centered" target="_blank" href="<?php esc_url( the_field( 'home_page_settings_about_us_button_1_link' ) ); ?>"><?php esc_html_e( 'Gästemappe', 'shs-a' ); ?></a>
 				<a class="main-btn main-btn__white main-btn__mob-centered" href="<?php esc_url( the_field( 'home_page_settings_about_us_button_2_link' ) ); ?>"><?php esc_html_e( 'Mehr erfahren', 'shs-a' ); ?></a>
 			</div>
 		</div>
