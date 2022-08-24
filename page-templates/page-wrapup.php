4ee0bacd3c68af6032f652ee8a2acdb82ee0b6d0
Daniel-Digid <daniel@dig.id>
2022-08-24 15:46:26 +0100

    Dev

---
+++ page-templates/page-wrapup.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Template Name: Wrap Up Page Template
+ *
+ * The template for displaying all content of the wrap up page
+ */
+
+get_header();
+	get_template_part( 'template-parts/wrapup/wrapup-page-intro' );
+get_footer();
---
+++ template-parts/wrapup/wrapup-page-intro.php
@@ -0,0 +1,15 @@
+<section id="wrapup-intro-section" class="section section__wrapupintro">
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-md-12 col-lg-12 col-xl-6 wrapup-intro__col p-0">
+                <img src="<?php the_field('wrap_up_section_wrap_up_section_image'); ?>" alt="" title="">
+            </div>
+			<div class="col-md-12 col-lg-12 col-xl-6">
+                <p class="wrapup-intro__ante-title"><span><?php the_field('wrap_up_section_wrap_up_section_subtitle'); ?></span></p>
+                <h2 class="wrapup-intro__title"><?php the_field('wrap_up_section_wrap_up_section_title'); ?></h2>
+                <p class="aboutus__text-intro"><?php the_field('wrap_up_section_wrap_up_section_description'); ?></p>
+                <a class="main-btn main-btn__red tooltip" href="<?php the_field('wrap_up_section_wrap_up_section_link'); ?>">Zu den Präsentationen<span class="tooltiptext">Präsentationen und Fotos des Swiss Innovation Day 2022 werden in kürze hier verfügbar sein.</span></a>
+            </div>
+		</div>
+	</div>
+</section>
