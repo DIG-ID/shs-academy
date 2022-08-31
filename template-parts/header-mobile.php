b5863b18c5458a00d9742a1f1cff97a0b5a6207a
Daniel-Digid <daniel@dig.id>
2022-08-31 17:01:33 +0100

    Dev

--- template-parts/header-mobile.php
+++ template-parts/header-mobile.php
@@ -1,5 +1,5 @@
 <div class="menu__rightside">
-	<a class="button__menured only__responsive  disabled-menu-button" href="/wrap-up/">Wrap Up</a>
+	<a class="button__menured only__responsive" href="/wrap-up/">Wrap Up</a>
 	<button id="opener" type="button" class="navbar-toggle only__responsive">
 		<span class="icon-bar"></span>
 		<span class="icon-bar"></span>
