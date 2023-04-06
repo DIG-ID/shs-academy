9be98f893067bd296c7434ecd6f09be65ad4aee5
pedro-digid <98165692+pedro-digid@users.noreply.github.com>
2023-04-06 17:44:17 +0100

    up

--- template-parts/events-menu.php
+++ template-parts/events-menu.php
@@ -1,11 +1,11 @@
 <div id="opener__eventsmenu" class="closed">
 	<div class="container menu__event-container">
 		<div class="row">
-			<div class="col-6 col-md-6 col-sm-12 px-15 menu__event-link" href="<?php echo get_permalink( get_page_by_path( 'tickets' ) ) ?>">
+			<a class="col-6 col-md-6 col-sm-12 px-15 menu__event-link" href="<?php echo get_permalink( get_page_by_path( 'tickets' ) ) ?>">
 					<p class="menu__event-title"><?php  echo get_the_title( get_page_by_path( 'tickets' ) ); ?></p>
 					<p class="menu__event-text">Sichere Dir Deinen Platz am #SID22 mit attraktiven Vergünstigungen.</p>
 					<p class="menu__event-button">MEHR ERFAHREN</p>
-			</div>
+			</a>
 			<a class="col-6 col-md-6 col-sm-12 px-15 menu__event-link" href="<?php echo get_permalink( get_page_by_path( 'programm' ) ) ?>">
 				<p class="menu__event-title"><?php  echo get_the_title( get_page_by_path( 'programm' ) ); ?></p>
 				<p class="menu__event-text">Mit dem Motto «ReDefining Hospitality» dreht sich alles um Talente, Nachhaltigkeit und Hoteltechnologie.</p>
