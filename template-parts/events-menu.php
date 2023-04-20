34bfa9abcbad0300efe114765f62f73bc6336b64
Daniel-Digid <daniel@dig.id>
2023-04-20 17:33:48 +0100

    dev

--- template-parts/events-menu.php
+++ template-parts/events-menu.php
@@ -1,11 +1,11 @@
 <div id="opener__eventsmenu" class="closed">
 	<div class="container menu__event-container">
 		<div class="row">
-			<div class="col-6 col-md-6 col-sm-12 px-15 menu__event-link disabled-menu-item" href="<?php echo get_permalink( get_page_by_path( 'tickets' ) ) ?>">
+			<a class="col-6 col-md-6 col-sm-12 px-15 menu__event-link" href="<?php echo get_permalink( get_page_by_path( 'tickets' ) ) ?>">
 				<p class="menu__event-title"><?php  echo get_the_title( get_page_by_path( 'tickets' ) ); ?></p>
 				<p class="menu__event-text">Sichere Dir Deinen Platz am #SID22 mit attraktiven Vergünstigungen.</p>
 				<p class="menu__event-button">MEHR ERFAHREN</p>
-			</div>
+			</a>
 			<a class="col-6 col-md-6 col-sm-12 px-15 menu__event-link" href="<?php echo get_permalink( get_page_by_path( 'programm' ) ) ?>">
 				<p class="menu__event-title"><?php  echo get_the_title( get_page_by_path( 'programm' ) ); ?></p>
 				<p class="menu__event-text">Unter dem Motto „The Future of Pricing“ werfen wir einen Blick hinter die Kulissen der psychologischen Entscheidungen, die bei Preisstrategien getroffen werden.</p>
--- template-parts/header-mobile.php
+++ template-parts/header-mobile.php
@@ -1,5 +1,5 @@
 <div class="menu__rightside">
-	<a class="button__menured only__responsive" href="/wrap-up/"><?php esc_html_e( 'Wrap Up 2022' ); ?></a>
+	<a class="button__menured only__responsive" href="/tickets/"><?php esc_html_e( 'Tickets' ); ?></a>
 	<button id="opener" type="button" class="navbar-toggle only__responsive">
 		<span class="icon-bar"></span>
 		<span class="icon-bar"></span>
