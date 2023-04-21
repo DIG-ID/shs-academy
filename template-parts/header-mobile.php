24dc0de5b30618ab1d684829b87db66ec6ef7a25
Daniel-Digid <daniel@dig.id>
2023-04-21 10:25:33 +0100

    dev

--- template-parts/header-mobile.php
+++ template-parts/header-mobile.php
@@ -1,5 +1,5 @@
 <div class="menu__rightside">
-	<a class="button__menured only__responsive" href="/tickets/"><?php esc_html_e( 'Tickets' ); ?></a>
+	<a class="button__menured only__responsive" href="/tickets/"><?php esc_html_e( 'Tickets kaufen' ); ?></a>
 	<button id="opener" type="button" class="navbar-toggle only__responsive">
 		<span class="icon-bar"></span>
 		<span class="icon-bar"></span>
