61bb078a73a0577b017eceda71d8d9c9ccb7a073
Daniel-Digid <daniel@dig.id>
2023-05-31 13:59:06 +0100

    dev

--- src/js/incert.js
+++ src/js/incert.js
@@ -1,5 +1,5 @@
 $(function() {
-  var iframe_src = "https://shsacademy.traumgutscheine.com/start.php?incertframe=new";
+  var iframe_src = "https://shsacademy.traumgutscheine.com/start.php?incertframe=new?sub_shop_id=78";
   (function(){
     var frameContainerID = 'myIncertIframe',
     landingPageURL = iframe_src;
