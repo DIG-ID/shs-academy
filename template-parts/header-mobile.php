cdc8b4bb27156a122c9b20a360d613aa2b751384
Daniel-Digid <daniel@dig.id>
2023-09-08 12:10:35 +0100

    d

--- template-parts/header-mobile.php
+++ template-parts/header-mobile.php
@@ -1,5 +1,5 @@
 <div class="menu__rightside">
-	<a class="button__menured only__responsive" href="https://guide.shsacademy.ch/" target="_blank"><?php esc_html_e( 'Gästemappe' ); ?></a>
+	<a class="button__menured only__responsive" href="/wrap-up/" target="_blank"><?php esc_html_e( 'Wrap up' ); ?></a>
 	<button id="opener" type="button" class="navbar-toggle only__responsive">
 		<span class="icon-bar"></span>
 		<span class="icon-bar"></span>
