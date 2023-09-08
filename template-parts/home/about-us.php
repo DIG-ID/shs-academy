b3a052a003eb273a4bd76a30e821135cea67789d
Daniel-Digid <daniel@dig.id>
2023-09-08 11:36:54 +0100

    d

--- template-parts/home/about-us.php
+++ template-parts/home/about-us.php
@@ -14,7 +14,7 @@
 						<?php the_field( 'home_page_settings_about_us_content' ); ?>
 				</div>
 				<p class="visitors-number"><span class="number"><?php the_field( 'home_page_settings_about_us_visitors_number' ); ?></span> <?php esc_html_e( 'Besucher', 'shs-a' ); ?></p>
-				<a class="main-btn main-btn__red main-btn__mob-centered" target="_blank" href="<?php esc_url( the_field( 'home_page_settings_about_us_button_1_link' ) ); ?>"><?php esc_html_e( 'Gästemappe', 'shs-a' ); ?>
+				<a class="main-btn main-btn__red main-btn__mob-centered" target="_blank" href="<?php esc_url( the_field( 'home_page_settings_about_us_button_1_link' ) ); ?>"><?php esc_html_e( 'Wrap-up', 'shs-a' ); ?>
 					<div class="tooltiptext-body">
 						<img src="<?php echo wp_upload_dir()['url'] . '/Top__bar.svg' ?>" width="45" height="45">
 						<span>Präsentationen und Fotos des Swiss Innovation Day 2022 werden in Kürze hier verfügbar sein.</span>
